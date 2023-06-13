9f5dfe2e12eb4b5c54ec0530387f13f6d7266fcf
Philip Degarmo <aclysma@gmail.com>
2023-06-13 11:39:31 -0700

    First pass setting up bindgen

---
+++ bindings_generator/wrapper.h
@@ -0,0 +1,16 @@
+
+// This wrapper #includes the metal_irconverter header with a few tweaks to make bindgen happy
+
+// Bindgen produces better output when building as C and not C++.
+typedef struct IRError IRError;
+typedef struct IRRootSignature IRRootSignature;
+typedef struct IRObject IRObject;
+typedef struct IRCompiler IRCompiler;
+typedef struct IRMetalLibBinary IRMetalLibBinary;
+typedef struct IRShaderReflection IRShaderReflection;
+typedef enum IRReflectionVersion IRReflectionVersion;
+
+// Undef __APPLE__ so we avoid generating bindings for dispatch/dispatch.h
+#undef __APPLE__
+
+#include <metal_irconverter/metal_irconverter.h>
