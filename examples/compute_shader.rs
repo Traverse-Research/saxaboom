4fb3e7b14b6a85958d8b4aa2111021e37685929d
Marijn Suijten <marijn@traverseresearch.nl>
2024-07-15 17:24:47 +0200

    Reduce number of `crates.io` keywords and split+improve README
    crates.io refuses crates with >5 keywords (we had 6).
    
    Also improve the readme, which was way too light (i.e. no code example
    showing how to use `saxaboom` and `saxaboom-runtime`).

--- examples/compute_shader.rs
+++ examples/compute_shader.rs
@@ -26,60 +26,59 @@ fn create_static_sampler(
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    unsafe {
-        // Load the library
-        let metal_irconverter = MetalIrConverter::new("libmetalirconverter.dylib").unwrap();
-        // Create an instance of IRCompiler
-        let mut compiler = metal_irconverter.create_compiler();
+    // Load the library
+    let metal_irconverter = MetalIrConverter::new("libmetalirconverter.dylib").unwrap();
+    // Create an instance of IRCompiler
+    let mut compiler = metal_irconverter.create_compiler();
 
-        // Create an explicit root signature layout
-        let mut parameters = create_root_parameters();
-        let mut static_samplers = create_static_samplers();
+    // Create an explicit root signature layout
+    let mut parameters = create_root_parameters();
+    let mut static_samplers = create_static_samplers();
 
-        let desc_1_1 = ffi::IRRootSignatureDescriptor1 {
-            Flags: ffi::IRRootSignatureFlags::CBVSRVUAVHeapDirectlyIndexed,
-            NumParameters: parameters.len() as u32,
-            pParameters: parameters.as_mut_ptr(),
-            NumStaticSamplers: static_samplers.len() as u32,
-            pStaticSamplers: static_samplers.as_mut_ptr(),
-        };
+    let desc_1_1 = ffi::IRRootSignatureDescriptor1 {
+        Flags: ffi::IRRootSignatureFlags::CBVSRVUAVHeapDirectlyIndexed,
+        NumParameters: parameters.len() as u32,
+        pParameters: parameters.as_mut_ptr(),
+        NumStaticSamplers: static_samplers.len() as u32,
+        pStaticSamplers: static_samplers.as_mut_ptr(),
+    };
 
-        let desc = ffi::IRVersionedRootSignatureDescriptor {
-            version: ffi::IRRootSignatureVersion::_1_1,
-            u_1: ffi::IRVersionedRootSignatureDescriptor_u { desc_1_1 },
-        };
+    let desc = ffi::IRVersionedRootSignatureDescriptor {
+        version: ffi::IRRootSignatureVersion::_1_1,
+        u_1: ffi::IRVersionedRootSignatureDescriptor_u { desc_1_1 },
+    };
 
-        let root_sig = metal_irconverter.create_root_signature_from_descriptor(&desc)?;
-        compiler.set_global_root_signature(&root_sig);
+    let root_sig = metal_irconverter.create_root_signature_from_descriptor(&desc)?;
+    compiler.set_global_root_signature(&root_sig);
 
-        // Load DXIL
-        let dxil = include_bytes!("assets/memcpy.cs.dxil");
-        let obj = metal_irconverter.create_object_from_dxil(dxil);
+    // Load DXIL
+    let dxil = include_bytes!("assets/memcpy.cs.dxil");
+    let dxil = metal_irconverter.create_object_from_dxil(dxil);
 
-        // Convert to Metal
-        let mtllib = compiler.alloc_compile_and_link(c"main", &obj)?;
-        let mtl_binary = mtllib
-            .metal_lib_binary()
-            .expect("Compiled object should contain a `metallib`");
+    // Convert to Metal
+    let mtllib = compiler.alloc_compile_and_link(c"main", &dxil)?;
+    let mtl_binary = mtllib
+        .metal_lib_binary()
+        .expect("Compiled object should contain a `metallib`");
 
-        // Get Metal bytecode
-        let metal_bytecode = mtl_binary.byte_code();
-        dbg!(metal_bytecode.len());
-        dbg!(mtllib.r#type());
-        dbg!(mtllib.metal_ir_shader_stage());
+    // Get Metal bytecode
+    let metal_bytecode = mtl_binary.byte_code();
+    dbg!(metal_bytecode.len());
+    dbg!(mtllib.r#type());
+    dbg!(mtllib.metal_ir_shader_stage());
 
-        // Get reflection from the shader
-        let mtl_reflection = mtllib.reflection();
+    // Get reflection from the shader
+    let mtl_reflection = mtllib.reflection();
 
-        let compute_info = mtl_reflection.map(|mtl_reflection| {
-            mtl_reflection
-                .compute_info(ffi::IRReflectionVersion::_1_0)
-                .unwrap()
-                .u_1
-                .info_1_0
-        });
-        dbg!(compute_info);
-    }
+    let compute_info = mtl_reflection.map(|mtl_reflection| unsafe {
+        mtl_reflection
+            .compute_info(ffi::IRReflectionVersion::_1_0)
+            .unwrap()
+            .u_1
+            .info_1_0
+    });
+    dbg!(compute_info);
+
     Ok(())
 }
 
