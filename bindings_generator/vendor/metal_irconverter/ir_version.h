ff388cd63e9f567b94584c9fc8dea49e3a64d102
Marijn Suijten <marijn@traverseresearch.nl>
2025-04-23 14:00:42 +0200

    Upgrade `metal_irconverter` headers and bindings to `2.1` (#35)
    Despite not having any code/header/binding changes, this update
    signifies to users that our release would be compatible with the latest
    `2.1` release.

--- bindings_generator/vendor/metal_irconverter/ir_version.h
+++ bindings_generator/vendor/metal_irconverter/ir_version.h
@@ -1,6 +1,6 @@
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------
 //
-// Copyright 2023-2024 Apple Inc.
+// Copyright 2023-2025 Apple Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -20,8 +20,8 @@
 #define IRVERSION_H
 
 #define IR_VERSION_MAJOR 2
-#define IR_VERSION_MINOR 0
-#define IR_VERSION_PATCH 5
+#define IR_VERSION_MINOR 1
+#define IR_VERSION_PATCH 0
 
 #define IR_SUPPORTS_VERSION(major, minor, patch) \
     ((major < IR_VERSION_MAJOR) || \
